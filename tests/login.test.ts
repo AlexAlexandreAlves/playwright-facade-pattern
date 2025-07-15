2e14df5a0769b6a0f37da3b2ebcb9bcded1eadf8
alex.alves <alexalexandrealves@gmail.com>
2025-07-14 22:50:20 -0300

    fix :hammer:

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -13,21 +13,15 @@ test.describe('Register and Authentication tests', () => {
         const register = new RegisterFacade(page);
         const auth = new AuthFacade(page);
 
+        // Do the registration
         await register.registerAs("usuarioteste@gmail.com",
             "Testezinho da Silva",
             "12345",
             "12345");
 
+        // Register confirmation assert
         await register.registerConfirmation();
-
         // Do login at the same flow because the application doesn't have database, so it saves only in memory
         await auth.loginAs("usuarioteste@gmail.com", "12345");
-
     });
-
-    // test('CT002 - Should be able to logging in with an exist user', async ({ page }) => {
-    //     const auth = new AuthFacade(page);
-    //     await auth.loginAs("usuarioteste@gmail.com", "12345");
-    // });
-
 });
